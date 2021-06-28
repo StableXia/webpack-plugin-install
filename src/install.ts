ef2b5a90f9ea6caffb059e46d03aa45f14bf3f38
Wen Xia <wen.xia@wework.com>
2021-06-28 18:02:24 +0800

    test

--- src/install.ts
+++ src/install.ts
@@ -5,12 +5,14 @@ type ICmdName = "npm" | "yarn";
 export interface ICmdOptions {
   cmd: ICmdName;
   args: string[];
+  cwd?: string;
 }
 
 export default function install(options: ICmdOptions) {
   const opts = {
     cmd: options.cmd,
     args: options.args,
+    cwd: options.cwd,
   };
 
   return runCommand(opts);
--- tests/installPlugin.test.js
+++ tests/installPlugin.test.js
@@ -1,2 +1,20 @@
-import webapck from "webapck";
-import InsatllPlugin from "../lib/index";
+const webapck = require("webpack");
+const InsatllPlugin = require("../lib/index").default;
+
+function testInsatllPlugin(webpackConfig, done) {
+  webapck(webpackConfig, (err, stats) => {
+    expect(err).toBeFalsy();
+    done();
+  });
+}
+
+describe("InsatllPlugin", () => {
+  it("使用 yarn 安装依赖", (done) => {
+    testInsatllPlugin(
+      {
+        plugins: [new InsatllPlugin({ cmd: "yarn", args: ["add", "webpack"] })],
+      },
+      done
+    );
+  });
+});
